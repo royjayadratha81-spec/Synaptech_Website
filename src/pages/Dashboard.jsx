5a0239219ce5f398c01a0f7de03d92e5f3935cd8
Jayadratha Roy <royjayadratha81@gmail.com>
2026-05-29 00:54:01 +0530

    Fixed firebase import path

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
 import Progress from "./Progress";
 import Profile from "./Profile";
